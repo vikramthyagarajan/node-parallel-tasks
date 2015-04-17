9dfed4b507136c218c0dcf3773074bd7a424380f
vikram <viksdegod@gmail.com>
2015-04-17 16:48:13 +0530

    Made servers which listen to sockets

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports={
+	port:8993,
+	lan:'192.168.2'
+};
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var winston=require('winston');
 var PortScanner=require('./lib/portscanner.js');
+var SocketCommunicator=require('./lib/socketcomm.js');
 
-var currentPortScanner=new PortScanner({lan:'192.168.1',port:'8000'});
-console.log('out')
-console.log(currentPortScanner.lan)
+var currentSocketComm=new SocketCommunicator();
+
+var currentPortScanner=new PortScanner({lan:'192.168.2',port:'8000'});
 currentPortScanner.scanPorts(function(err,connectedMachines){console.log(connectedMachines)});
-currentPortScanner.lan;
+
