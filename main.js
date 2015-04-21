c552a84914fdf7d8ed483e820031f676c2e91f61
vikram <viksdegod@gmail.com>
2015-04-21 17:07:09 +0530

    Fixed the cyclic dependency requires

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var winston=require('winston');
 var config=require('./config/config.js');
-var ExecutionEngine=require('./lib/executionengine.js');
 var SocketCommunicator=require('./lib/socketcomm.js');
+var ExecutionEngine=require('./lib/executionengine.js');
 var PortScanner=require('./lib/portscanner.js');
 
 var currentPortScanner=new PortScanner({lan:config.lan,port:config.port});
@@ -9,8 +9,8 @@ currentPortScanner.scanPorts(function(err,connectedMachines){
 	console.log('Connected Machines are-');
 	console.log(connectedMachines)
 	console.log(SocketCommunicator);
-	var executionEngine=ExecutionEngine.getExecutionEngine();
 	var currentSocketComm=SocketCommunicator.getSocketCommunicator();
+	var executionEngine=ExecutionEngine.getExecutionEngine(currentSocketComm);
 	currentSocketComm.connectTo(connectedMachines,function(){
 		console.log('connected is done');
 		executionEngine.executeMapReduce([1,2,3,4],function(elem,innerC) {
