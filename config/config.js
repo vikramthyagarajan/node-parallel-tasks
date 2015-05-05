d1aa5d9b6eb4c28edb5aabf4d6ea3da68693017b
vikram thyagarajan <vikramthyagarajan@vikrams-MacBook-Pro.local>
2015-05-05 12:57:52 +0530

    Recognizes self and starts negotiation properly

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports={
 	port:8000,
-	lan:'192.168.2',
+	lan:'192.168.0',
 	//time the ExecutionEngine will wait for receiving resourceAvailable messages
 	resourceAcquisitionTimeout:5000
 };
--- main.js
+++ main.js
@@ -4,13 +4,12 @@ var SocketCommunicator=require('./lib/socketcomm.js');
 var ExecutionEngine=require('./lib/executionengine.js');
 var PortScanner=require('./lib/portscanner.js');
 
+var currentSocketComm=SocketCommunicator.getSocketCommunicator();
 var currentPortScanner=new PortScanner({lan:config.lan,port:config.port});
+var executionEngine=ExecutionEngine.getExecutionEngine(currentSocketComm);
 currentPortScanner.scanPorts(function(err,connectedMachines){
 	console.log('Connected Machines are-');
 	console.log(connectedMachines)
-	console.log(SocketCommunicator);
-	var currentSocketComm=SocketCommunicator.getSocketCommunicator();
-	var executionEngine=ExecutionEngine.getExecutionEngine(currentSocketComm);
 	currentSocketComm.connectTo(connectedMachines,function(){
 		console.log('connected is done');
 		executionEngine.executeMapReduce([1,2,3,4],function(elem,innerC) {
