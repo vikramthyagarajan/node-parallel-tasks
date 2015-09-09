007d32f3b34e3f90bf53f2a1d4af5f891b707fb1
Vikram Thyagarajan <vikram@ptotem.com>
2015-09-09 17:58:49 +0530

    added documentation for git pages

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-	port:8000,
-	lan:'192.168.0',
-	//time the ExecutionEngine will wait for receiving resourceAvailable messages
-	resourceAcquisitionTimeout:5000
-};
--- examples/async-example1.js
+++
@@ -1,8 +0,0 @@
-var asyncParallel=require('../main.js');
-
-asyncParallel.executionEngine.executeMapReduce([1,2,3,4],function(elem,innerC) {
-	innerC(elem+1);
-},function(err,results) {
-	console.log('got results');
-	console.log(results);
-});
