e61b048ae0a0adda611edef3b9da108f40d5a0f5
Vikram Thyagarajan <vikram@ptotem.com>
2016-01-07 20:25:07 +0530

    Allow to run as script. --daemon to run as Deamon

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
 var winston=require('winston');
+var async = require('async');
+var _ = require('lodash');
+var portastic = require('portastic');
+var fs = require('fs');
+
 var config=require('./config/config.js');
 var SocketCommunicator=require('./lib/socketcomm.js');
 var ExecutionEngine=require('./lib/executionengine.js');
 var PortScanner=require('./lib/portscanner.js');
-var async = require('async');
 
-var currentSocketComm=SocketCommunicator.getSocketCommunicator();
-var currentPortScanner=new PortScanner({lan:config.lan,port:config.port});
-var executionEngine=ExecutionEngine.getExecutionEngine(currentSocketComm);
+var args = process.argv;
+var isDaemon = _.includes(args, "--daemon");
+if (isDaemon) {
+	winston.info('preparing to run the app as daemon');
+	winston.info(process.pid);
+	//before forking the process, checking if port is available
+	portastic.test(config.port).then( function(isOpen) {
+		if (isOpen) {
+			require('daemon')({
+				stderr: fs.openSync('./log/err.log', 'a'),
+				stdout: fs.openSync('./log/out.log', 'a')
+			});
+			initializeEnvironment();
+		}
+		else {
+			winston.info('Port '+ config.port +' is not available. Daemon not created');
+			process.exit(0);
+		}
+	});
+	console.log(process.pid);
+}
+else initializeEnvironment();
 
-var queue = async.queue(function(task, callback) {
-	winston.info('running task on the queue');
-	task(callback);
-});
-queue.pause();
+var currentSocketComm, currentPortScanner, executionEngine, queue;
+
+/**
+ * Function that initializes all required steps to get node-parallel tasks up and listening for requests
+ *
+ */
+function initializeEnvironment() {
+	currentSocketComm=SocketCommunicator.getSocketCommunicator();
+	currentPortScanner=new PortScanner({lan:config.lan,port:config.port});
+	executionEngine=ExecutionEngine.getExecutionEngine(currentSocketComm);
+	queue = async.queue(function(task, callback) {
+		winston.info('running task on the queue');
+		task(callback);
+	});
+	queue.pause();
+	initializeEngine();
+}
+
+
 /**
  * Function that scans and connects to all available machines in the local network. Required befor doing all operations
  *
@@ -29,7 +66,6 @@ function initializeEngine() {
 		});
 	});
 };
-initializeEngine();
 
 module.exports = {
 	mapReduce: function(arr, map, reduce) {
