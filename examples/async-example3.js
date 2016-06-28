3a97182f2db5aa64c5f44ff2b715f1304063f87c
rowhanm <rohanmahadev7@gmail.com>
2016-06-28 12:09:48 +0530

    Async changes

--- examples/async-example3.js
+++ examples/async-example3.js
@@ -1,8 +1,9 @@
 var asyncParallel=require('../main.js');
 
-asyncParallel.parallel(function() {
+asyncParallel.parallel(function(cb) {
 	setTimeout(function() {
 		console.log("Executing locally.");
+		cb();
 	}, 2000);
 }, function(err,res) {
 	console.log("Done executing on all machines");
