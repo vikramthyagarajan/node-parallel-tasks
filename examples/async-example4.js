c19864ae53d7a1a0066bb1e1229030eccfc51a46
rowhanm <rohanmahadev7@gmail.com>
2016-06-28 18:39:01 +0530

    Parallel Changes

--- examples/async-example4.js
+++ examples/async-example4.js
@@ -1,17 +1,30 @@
 var asyncParallel=require('../main.js');
 var arr = [];
-var function1 = function(){
+var function1 = function(cb){
   console.log("Printing Function 1");
+  cb();
 }
 
-var function2 = function(){
-  console.log("Printing Function 2");
+var function2 = function(cb){
+  setTimeout(function(){
+    console.log("Printing Function 2");
+    cb();
+  },1000);
 }
+var function3 = function(cb){
+  console.log("Printing Function 3");
+  cb();
+}
+var function4 = function(cb){
+  console.log("Printing Function 4");
+  cb();
+}
+
 
 arr.push(function1);
 arr.push(function2);
-arr.push(function1);
-arr.push(function2);
+arr.push(function3);
+arr.push(function4);
 
 asyncParallel.parallel(arr, function(cb) {
 	setTimeout(function() {
